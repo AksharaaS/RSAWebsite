8cd0ffa15cd38de2a76e1f0bab1e863abddabfce
AksharaaS <aksharaa@rsalesarm.com>
2022-08-04 00:47:48 +0530

    Updated website

--- assets/js/main.js
+++ assets/js/main.js
@@ -237,6 +237,54 @@
     }
   });
 
+
+  /**
+   * Top menu scroll
+  */
+
+  const triggers = document.querySelectorAll('.top-menu > li');
+  const background = document.querySelector('.dropdownBackground')
+  const nav = document.querySelector('.navbar')
+
+  function handleEnter(){
+    console.log('Enter!');
+    this.classList.add('trigger-enter');
+    setTimeout(()=>this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'),150);
+    background.classList.add('open');
+
+    const dropdown = this.querySelector('.dropdownOne')
+    const dropdownCoords = dropdown.getBoundingClientRect();
+    const navCoords = nav.getBoundingClientRect();
+
+    const coords = {
+      height: dropdownCoords.height,
+      width: dropdownCoords.width,
+      top: dropdownCoords.top - navCoords.top,
+      left: dropdownCoords.left - navCoords.left,
+    };
+
+    background.style.setProperty('width', `${coords.width}px`);
+    background.style.setProperty('height', `${coords.height}px`);
+    background.style.setProperty('transform', `translate(${coords.left}px, ${coords.top}px`);
+
+
+  }
+  function handleLeave(){
+    console.log('Leave!');
+    this.classList.remove('trigger-enter', 'trigger-enter-active');
+    background.classList.remove('open');
+
+
+
+  }
+
+  
+  triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
+  triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
+
+
+
+
   /**
    * Animation on scroll
    */
@@ -250,3 +298,14 @@
   });
 
 })()
+
+
+let parent = document.querySelector('.sticky').parentElement;
+
+while (parent) {
+    const hasOverflow = getComputedStyle(parent).overflow;
+    if (hasOverflow !== 'visible') {
+        console.log(hasOverflow, parent);
+    }
+    parent = parent.parentElement;
+}
---
+++ sendEmail.php
@@ -0,0 +1,54 @@
+<?php 
+
+header('Access-Control-Allow-Origin: *');
+
+use PHPMailer\PHPMailer\PHPMailer;
+
+if (isset($_POST['fname']) && isset($_POST['lname']) && isset($_POST['email']) && isset($_POST['number'])) {
+	$fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$email = $_POST['email']; 
+	$number = $_POST['number']; 
+	$subject = "has sent an enquiry";
+	$company_website = $_POST['company_website'];
+	$company_size = $_POST['company_size'];
+	$message = $_POST['message'];
+	
+
+	require_once "PHPMailer/PHPMailer.php";
+	require_once "PHPMailer/SMTP.php";
+	require_once "PHPMailer/Exception.php";
+
+	$mail= new PHPMailer();
+
+	$mail->isSMTP();
+	$mail->Host = "smtp.gmail.com";
+	$mail->SMTPAuth = true;
+	$mail->Username = "info@rsalesarm.com";
+	$mail->Password = "Subbu12345^";
+	$mail->Port=465;
+	$mail->SMTPSecure="ssl";
+
+	// email settings
+	$mail->isHTML(true);
+	$mail->setFrom($email,$name);
+	$mail->setAddress("info@rsalesarm.com");
+	$mail->Subject = ($email ($subject));
+	$mail->Body= $fname;
+
+	if ($mail->send()) {
+
+		$status="success";
+		$response = "Email is sent!";
+	}
+	else {
+		$status="failed";
+		$response = "Something is wrong: <br>" . $mail->ErrorInfo;
+	}
+	exit(json_encode(array("status"=>$status, "response"=>$response)));
+
+}
+
+
+
+?>
